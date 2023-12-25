6caaa5871e00ced805e3bd19ce2601977a140ee3
Jonathan Tong <jontong321@gmail.com>
2023-12-25 14:41:00 -0500

    Updated change

--- SwiftUI-Weather/WeatherButton.swift
+++ SwiftUI-Weather/WeatherButton.swift
@@ -4,5 +4,21 @@
 //
 //  Created by Jonathan on 2023-12-25.
 //
-
+import SwiftUI
 import Foundation
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+    }
+}
